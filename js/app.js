99eb90682f2e266c9550fe46250562a8d6d87ed6
Fraser McLaughlin <fraser.mclaughlin@outlook.com>
2022-06-27 15:17:48 +0200

    Info popup almost working, just not on touch screens?

--- js/app.js
+++ js/app.js
@@ -3,11 +3,29 @@
 const shinybtn = document.querySelector('.btn--shiny');
 const shinymodal = document.querySelector('.shiny-intro-modal');
 
+const shinyinfobtn = document.querySelector('.shiny-box__button');
+const shinyinfo = document.querySelector('.shiny-info-modal');
+const shinyback = document.querySelector('.btn--shiny--back');
+const mapapp = document.querySelector('iframe');
+
+// MOVE PAST LANDING PAGE
 function handleClick() {
 	shinymodal.style.display = 'none';
 }
-
 shinybtn.addEventListener('click', handleClick);
+
+// TOGGLE INFO MODAL ON/OFF
+function handleShinyInfoON() {
+	shinyinfo.style.display = 'flex';
+	mapapp.height = '0';
+}
+function handleShinyInfoOFF() {
+	shinyinfo.style.display = 'none';
+	mapapp.height = '100%';
+}
+
+shinyinfobtn.addEventListener('click', handleShinyInfoON);
+shinyback.addEventListener('click', handleShinyInfoOFF);
 
 // NOT WORKING
 // shinybtn.addEventListener('click', function () {
